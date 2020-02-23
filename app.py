2fb1dc00d61d660ddd642f6e6c1d77be5f11b0f8
Arbo Lindma <arbo.lindma@gmail.com>
2020-02-23 14:21:07 +0200

    Added redirect bact to index

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/save', methods=['POST'])
+def save():
+    return redirect(url_for('index'))
+
 
 app.run(host="0.0.0.0", port="8000", debug=True)
